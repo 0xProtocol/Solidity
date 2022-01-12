a709d004568c2fc193091829f9f434789d2faa70
"chrop01" <"christoph.oprawill@outlook.com">
2022-01-12 11:56:10 +0100

    Wei/Eth

--- smartcontract.sol
+++ smartcontract.sol
@@ -1,15 +1,21 @@
 pragma solidity ^0.5.3;
 
 contract Contract {
-    string public txt;
+    //string public txt;
+    uint public oneWei = 1 wei;
+    uint public oneEther = 1 ether;
 
-    function set(string memory _text) public{
+   /* function set(string memory _text) public{
         txt = _text;
-
     }
     function get() public view returns (string memory) {
         return txt;
+    }*/
+    function testOneWei() public pure returns (bool)
+    {return 1 wei == 1;}
 
-    }
+    function testOneEther() public pure returns (bool)
+    {return 1 ether == 1e18 wei;}
+    
 
 }
