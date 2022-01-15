69da88313962cdd67021e9f9a0c19a2c5b98cbef
"chrop01" <"christoph.oprawill@outlook.com">
2022-01-15 20:17:00 +0100

    gas fees

--- smartcontract.sol
+++ smartcontract.sol
@@ -4,6 +4,7 @@ contract Contract {
     //string public txt;
     uint public oneWei = 1 wei;
     uint public oneEther = 1 ether;
+    uint public i = 0;
 
    /* function set(string memory _text) public{
         txt = _text;
@@ -16,6 +17,18 @@ contract Contract {
 
     function testOneEther() public pure returns (bool)
     {return 1 ether == 1e18 wei;}
+
+    function gasRefund() public returns (uint)
+    {
+        return tx.gasprice; //fixed at 1 wei
+    }
+
+    function loop() public {
+        while(true)
+        {
+            i+=1;
+        }
+    }
     
 
 }
